0822b43ffb16e603896ef8e506c32b0064a3081f
Albin Varghese <albin8285@gmail.com>
2020-09-05 01:27:45 +0530

    Increased SEO

--- index.php
+++ index.php
@@ -5,15 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <link href="css/styles.css" rel="stylesheet">
+    <meta name="og:title" property="og:title" content="QR Code Generator">
+    <meta name="description" content="Simple QR code generator created using php.">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="manifest" href="/site.webmanifest">
     </head>
     <body class="bg-gray-800">
     	<div class="w-full max-w">
   <div class="flex items-center border-b border-teal-500 py-2 ">
     <div class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-bold text-xl">QR Generator</div>
-    <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
-      Share Repo
-    </button>
+    <a href="mailto:albinmelazhakathu@gmail.com"><button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+      Contact
+    </button></a>
     <button class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-teal-500 hover:border-teal-700 text-sm text-white text-sm py-1 px-2 rounded-full mx-3" type="button">
  	<i style="font-size: 27px;" class="fa fa-github"></i>
      </button>
@@ -59,7 +62,7 @@
         </div>
   
       <button name ="submit" class="text-white bg-teal-500 border-0 py-2 px-8 focus:outline-none hover:bg-teal-600 rounded text-lg mt-6">Generate</button>
-      <p class="text-xs text-center text-gray-500 mt-3">Please use simple text for conversation to prevent lagging. Thanks for Downloading my Project 😊</p>
+      <p class="text-xs text-center text-gray-500 mt-3">Please use simple text for conversation to prevent lagging. Thank you ☺️</p>
     </div></div>
     </form>
              <div></div>
--- result.php
+++ result.php
@@ -7,14 +7,18 @@
     <meta name="theme-color" content="#000000" />
     <link href="css/styles.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <meta name="og:title" property="og:title" content="QR Code Generator">
+    <meta name="description" content="Simple QR code generator created using php.">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="manifest" href="/site.webmanifest">
     </head>
     <body class="bg-gray-800">
     	<div class="w-full max-w">
   <div class="flex items-center border-b border-teal-500 py-2 ">
     <div class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-bold text-xl">QR Generator</div>
-    <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
-      Share Repo
-    </button>
+   <a href="mailto:albinmelazhakathu@gmail.com"><button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+      Contact
+    </button></a>
     <button class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-teal-500 hover:border-teal-700 text-sm text-white text-sm py-1 px-2 rounded-full mx-3" type="button">
  	<i style="font-size: 27px;" class="fa fa-github"></i>
      </button>
@@ -30,11 +34,12 @@
     
       <div class="text-center m-2">
         <a href="<?= htmlentities($result); ?>" download><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded">Download</button></a>
-		<a href="<?= htmlentities($result); ?>"><button class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 ml-2 text-sm rounded">View QR</button></a>
+		<a href="<?= htmlentities($result); ?>"><button class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 ml-2 text-sm rounded">View</button></a>
 
         </div>
       
-      <p class="text-xs text-center text-gray-500 mt-3">Designed by <a href="https://albinvarghese.tk"> Albin Varghese</a></p>
+      <p class="text-xs text-center text-gray-500 mt-3">Designed by <a href="https://albinvarghese.tk" target="_blank"> Albin Varghese</a></p>
+      <h4 class="text-xs text-center text-indigo-500 mt-3"><a href="index.php"> Go Back</a></h4>
     </div></div>
              <div></div>
     	</div>
