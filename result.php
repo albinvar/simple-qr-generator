eee633544061d8ebe71cea232940f53321b80bd7
Albin Varghese <albin8285@gmail.com>
2020-09-04 22:49:36 +0530

    Add result page (Design only)

---
+++ result.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <link href="css/styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body class="bg-gray-800">
+    	<div class="w-full max-w">
+  <div class="flex items-center border-b border-teal-500 py-2 ">
+    <div class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-bold text-xl">QR Generator</div>
+    <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+      Share Repo
+    </button>
+    <button class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-teal-500 hover:border-teal-700 text-sm text-white text-sm py-1 px-2 rounded-full mx-3" type="button">
+ 	<i style="font-size: 27px;" class="fa fa-github"></i>
+     </button>
+  </div>
+</div>
+	     <div class="px-8 py-12">
+    	     <div> 
+<div class="bg-gray-200 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+      <h2 class="text-center text-gray-900 text-lg font-medium title-font mb-5">QR Code Generator</h2>
+     
+    <img class="text-center mx-auto w-auto h-auto" src="example/qrcode.png">
+      <p class="text-xs text-center text-gray-500 mt-3">Your QR Code was generated successfully...!!!</p>
+    
+      <div class="text-center m-2">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded">Download</button>
+		<button class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 ml-2 text-sm rounded">View QR</button>
+
+        </div>
+      
+      <p class="text-xs text-center text-gray-500 mt-3">Designed by <a href="https://albinvarghese.tk"> Albin Varghese</a></p>
+    </div></div>
+             <div></div>
+    	</div>
+    </body>
+</html>
