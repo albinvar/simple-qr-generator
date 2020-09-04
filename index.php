67f98987e26505e5950eb2a9b9f7ffd6a94e6903
Albin Varghese <albin8285@gmail.com>
2020-09-04 21:20:44 +0530

    Add best Design

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <link href="css/styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body class="bg-gray-800">
+    	<div class="w-full max-w">
+  <div class="flex items-center border-b border-teal-500 py-2 ">
+    <div class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-bold text-xl">QR Generator</div>
+    <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+      Share Repo
+    </button>
+    <button class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-teal-500 hover:border-teal-700 text-sm text-white text-sm py-1 px-2 rounded-full mx-3" type="button">
+ 	<i style="font-size: 27px;" class="fa fa-github"></i>
+     </button>
+  </div>
+</div>
+	     <div class="px-8 py-12">
+    	     <div> 
+
+ <div class="bg-gray-200 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+      <h2 class="text-center text-gray-900 text-lg font-medium title-font mb-5">QR Code Generator</h2>
+      <div class="relative mb-4">
+        <label for="text" class="leading-7 text-sm text-gray-600">Text/Link</label>
+        <input type="text" id="text" name="text" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div class="lg:w-full md:w-2/3 mx-auto">
+      <div class="flex flex-wrap -m-2">
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="name" class="leading-7 text-sm text-gray-600">Padding
+           <select class="form-select block-sm w-full text-sm text-gray-600">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+  </select></label>
+          </div>
+        </div>
+        <div class="p-2 w-1/2">
+          <div class="relative">
+                  <label for="name" class="leading-7 text-sm text-gray-600">Clarity
+           <select class="form-select block-sm w-full text-sm text-gray-600">
+    <option value="1">1</option>
+    <option value="4">4</option>
+    <option value="6">6</option>
+    <option value="9">9</option>
+    <option value="15">15</option>
+  </select></label>
+          </div>
+        </div>
+        </div>
+        </div>
+  
+      <button class="text-white bg-teal-500 border-0 py-2 px-8 focus:outline-none hover:bg-teal-600 rounded text-lg mt-6">Generate</button>
+      <p class="text-xs text-gray-500 mt-3">Please use simple text for conversation to prevent lagging.</p>
+    </div></div>
+             <div></div>
+    	</div>
+    </body>
+</html>
