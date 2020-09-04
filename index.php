b13f62c67f05c944d5f019d1c8a83fa1e978829f
Albin Varghese <albin8285@gmail.com>
2020-09-05 01:50:40 +0530

    Added Animations

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <link href="css/styles.css" rel="stylesheet">
+    <link href="css/animations.css" rel="stylesheet">
     <meta name="og:title" property="og:title" content="QR Code Generator">
     <meta name="description" content="Simple QR code generator created using php.">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -17,7 +18,7 @@
     <a href="mailto:albinmelazhakathu@gmail.com"><button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
       Contact
     </button></a>
-    <button class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-teal-500 hover:border-teal-700 text-sm text-white text-sm py-1 px-2 rounded-full mx-3" type="button">
+    <button class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-teal-500 hover:border-teal-700 text-sm text-white text-sm py-1 px-2 rounded-full mx-3 heartbeat" type="button">
  	<i style="font-size: 27px;" class="fa fa-github"></i>
      </button>
   </div>
@@ -26,7 +27,7 @@
     	     <div> 
 <form action="result.php" method="post">
  <div class="bg-gray-200 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-      <h2 class="text-center text-gray-900 text-lg font-medium title-font mb-5">QR Code Generator</h2>
+      <h2 class="text-center text-gray-900 text-lg font-medium title-font mb-5 tracking-in-expand">QR Code Generator</h2>
       <div class="relative mb-4">
         <label for="text" class="leading-7 text-sm text-gray-600">Text/Link</label>
         <input type="text" id="text" name="text" placeholder="Type your text here" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
@@ -61,7 +62,7 @@
         </div>
         </div>
   
-      <button name ="submit" class="text-white bg-teal-500 border-0 py-2 px-8 focus:outline-none hover:bg-teal-600 rounded text-lg mt-6">Generate</button>
+      <button name ="submit" class="text-white bg-teal-500 border-0 py-2 px-8 focus:outline-none hover:bg-teal-600 rounded text-lg mt-6 bounce-bottom">Generate</button>
       <p class="text-xs text-center text-gray-500 mt-3">Please use simple text for conversation to prevent lagging. Thank you ☺️</p>
     </div></div>
     </form>
--- result.php
+++ result.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <link href="css/styles.css" rel="stylesheet">
+    <link href="css/animations.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    <meta name="og:title" property="og:title" content="QR Code Generator">
     <meta name="description" content="Simple QR code generator created using php.">
@@ -19,7 +20,7 @@
    <a href="mailto:albinmelazhakathu@gmail.com"><button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
       Contact
     </button></a>
-    <button class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-teal-500 hover:border-teal-700 text-sm text-white text-sm py-1 px-2 rounded-full mx-3" type="button">
+    <button class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-teal-500 hover:border-teal-700 text-sm text-white text-sm py-1 px-2 rounded-full mx-3 heartbeat" type="button">
  	<i style="font-size: 27px;" class="fa fa-github"></i>
      </button>
   </div>
@@ -29,12 +30,12 @@
 <div class="bg-gray-200 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
       <h2 class="text-center text-gray-900 text-lg font-medium title-font mb-5">QR Code Generator</h2>
      
-    <img class="text-center mx-auto w-auto h-auto" src="<?= htmlentities($result); ?>">
+    <img class="text-center mx-auto w-auto h-auto rotate-in-center" alt="qrcode" src="<?= htmlentities($result); ?>">
       <p class="text-xs text-center text-gray-500 mt-3">Your QR Code was generated successfully...!!!</p>
     
       <div class="text-center m-2">
-        <a href="<?= htmlentities($result); ?>" download><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded">Download</button></a>
-		<a href="<?= htmlentities($result); ?>"><button class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 ml-2 text-sm rounded">View</button></a>
+        <a href="<?= htmlentities($result); ?>" download><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 text-sm px-4 mr-2 rounded bounce-left">Download</button></a>
+		<a href="<?= htmlentities($result); ?>"><button class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 ml-2 text-sm rounded bounce-right">View</button></a>
 
         </div>
       
